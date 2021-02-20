0a180f7b81ee430c584aea845fff070a276351dc
TheYushinator <ayushtvora@gmail.com>
2021-02-19 23:48:41 -0500

    added old stuff

---
+++ js/app.js
@@ -0,0 +1,27 @@
+(function() {
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    var firebaseConfig = {
+        apiKey: "AIzaSyBwytOWnI8AqaVTf9LUrc4nWIk4p9-3Scw",
+        authDomain: "uofthacks2021-298a3.firebaseapp.com",
+        databaseURL: "https://uofthacks2021-298a3-default-rtdb.firebaseio.com",
+        projectId: "uofthacks2021-298a3",
+        storageBucket: "uofthacks2021-298a3.appspot.com",
+        messagingSenderId: "63017197391",
+        appId: "1:63017197391:web:87ea5611aecc4d95a12f3a",
+        measurementId: "G-L6NQG7YLL7"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+
+    // Get elements
+    const preObject = document.getElementById('object');
+
+    // Create references
+    const dbRefObject = firebase.database().ref().child('object');
+
+    // Sync object changes
+    dbRefObject.on('value', snap => console.log(snap.val()));
+}()); 
